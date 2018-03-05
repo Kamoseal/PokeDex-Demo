441fb16a09b0bc634f21cc1550bb4cb0395ccffc
Kamoseal <serna.karla@gmail.com>
2018-03-05 00:24:18 +0000

    Pintando datos en consola

--- js/app.js
+++ js/app.js
@@ -4,15 +4,27 @@ $.ajax({
 
 // Esta informacion la arroja a la funcion data Pokemon
 function dataPokemon (data){
+    //Ingresamos a la data en subrama Pokemon_entries
     const pokemonEntries = data.pokemon_entries;
-    console.log(pokemonEntries);
-    pokemonEntries.forEach(function(arraypokemonEntries) {
-       
+    //console.log(pokemonEntries);
+    //Recorremos los 721 Pokemon
 
-        console.log(arraypokemonEntries);
-        
+    let output = ``; 
+
+    pokemonEntries.forEach(function(arraypokemonEntries) { 
+        //Arroja la informacion de cada pokemon URL
+       const pokemonUrl = arraypokemonEntries.pokemon_species.url;
+       //Devuelve todos los nombres de los pokemon
+       const pokemonName = arraypokemonEntries.pokemon_species.name;
+         
+        output += `<div class="left"> <img src="${"https://dummyimage.com/300.png/09f/fff"}" alt="Pokemon"><p class="text-center">${pokemonName}</p><p><a href="${pokemonUrl}">${pokemonUrl}</a></p></div>`;
+
+        $("#container").html(output);
+        console.log(pokemonUrl); 
     });
-    let output = ``;
+    
+    
+    
 
 
 
